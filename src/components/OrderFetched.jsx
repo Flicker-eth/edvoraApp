ec2687232237eb81997f18cc1238be40e9d54c85
Flicker786 <69352034+Flicker786@users.noreply.github.com>
2022-08-19 17:58:25 +0530

    Initial commit

---
+++ src/components/OrderFetched.jsx
@@ -0,0 +1,66 @@
+import React, { useEffect,useState } from 'react';
+import styles from '../style';
+
+
+
+
+const OrderFetched = () => {
+  const [data,setdata] =  useState([]);
+   const getUserdata = async() => {
+       const res =await fetch("https://assessment.api.vweb.app/users");
+       const currentdata =  await res.json();
+       setdata(currentdata[0]);
+       console.log(currentdata[0]);
+   }
+       useEffect(() =>{
+            getUserdata();
+
+
+       },[])
+
+
+  return (
+
+<div className="w-full rounded-lg   shadow-md ">
+   
+    
+    <div  className=" border-blue-500">
+        <div className="p-4 bg-black-gradient rounded-lg md:p-8 " >
+        
+            <dl className="grid grid-cols-2 gap-8 p-4 mx-auto max-w-screen-xl text-gray-900 sm:grid-cols-3 xl:grid-cols-6 dark:text-white sm:p-8">
+                <div className="flex flex-col justify-center items-center">
+                    <dt className="mb-2 text-3xl font-extrabold">{data.user_id}</dt>
+                    <dd className="font-light text-gray-500 dark:text-gray-400">Quantity</dd>
+                </div>
+                <div className="flex flex-col justify-center items-center">
+                    <dt className="mb-2 text-3xl font-extrabold">100M+</dt>
+                    <dd className="font-light text-gray-500 dark:text-gray-400">Product Name</dd>
+                </div>
+                <div className="flex flex-col justify-center items-center">
+                    <dt className="mb-2 text-3xl font-extrabold">1000s</dt>
+                    <dd className="font-light text-gray-500 dark:text-gray-400">Order Date</dd>
+                </div>
+                <div className="flex flex-col justify-center items-center">
+                    <dt className="mb-2 text-3xl font-extrabold">{data.name}</dt>
+                    <dd className="font-light text-gray-500 dark:text-gray-400">UserName</dd>
+                </div>
+                <div className="flex flex-col justify-center items-center">
+                    <dt className="mb-2 text-3xl font-extrabold">90+</dt>
+                    <dd className="font-light text-gray-500 dark:text-gray-400">Product Listed</dd>
+                </div>
+                <div className="flex flex-col justify-center items-center">
+                    <dt className="mb-2 text-3xl font-extrabold">4M+</dt>
+                    <dd className="font-light text-gray-500 dark:text-gray-400">Members</dd>
+                </div>
+            </dl>
+        
+        </div>
+        
+       
+    </div>
+</div>
+
+  )
+}
+
+export default OrderFetched;
